fe4818ae9b31e3bb7ab35b01938af59b2ede3438
SEUNGHEE <did030930@gmail.com>
2024-03-16 11:37:39 +0900

    2024-3-14

---
+++ 2week/ex2.py
@@ -0,0 +1,9 @@
+str1="Python Program!"
+print(str1)
+print(str1[0],str1[7],str1[14])
+print(str1[-1],str1[-8],str1[-15])
+
+#인덱스 범위 지정 부분문자열 참조
+print(str1[0:6],str1[10:14])
+print(str1[7:])
+print(str1[:6])
