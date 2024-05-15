92a9d5d1a08bf54c21cd8ee692f7bde9c2761205
yangfaring <did030930@gmail.com>
2024-05-15 22:46:32 +0900

    Update ex1.py

--- 2week/ex1.py
+++ 2week/ex1.py
@@ -5,13 +5,13 @@ print("이름:",name,"평균:",avg,sep="*****",end="\t")
 name="차범근"; avg=90.3
 print("이름:",name,"평균:",avg)
 print("출력을 종료합니다!")
-
+#문자열 형식으로 출력됨
 print("2개의 숫자를 입력하세요.")
 num1=input()
 num2=input()
 num3=num1+num2
 print(num3)
-
+#연산되어 출력됨
 print("2개의 숫자를 입력하세요.")
 num1=int(input())
 num2=int(input())
