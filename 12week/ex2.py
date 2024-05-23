3523fa5bb9a11e72d7164eb8f21f5e9b866c83c3
yangfaring <did030930@gmail.com>
2024-05-23 20:48:49 +0900

    Update ex2.py

--- 12week/ex2.py
+++ 12week/ex2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import mariadb
-
+#DB 연결
 conn = mariadb.connect(
     user="user025",
     password="!ai123",
@@ -8,14 +8,14 @@ conn = mariadb.connect(
     port=53306,
     database="edudb"
 )
-cusor = conn.cursor()
-
+cusor = conn.cursor() #커서 생성
+# 검색 부분
 cusor.execute('SELECT * FROM city')
 results = cusor.fetchall()
 for row in results:
     print(row)
-
+#DataFrame으로 결과 변환
 df = pd.DataFrame(results)
 print(df.head(10), end="\n\n")
-
+# 연결 종료
 conn.close()
