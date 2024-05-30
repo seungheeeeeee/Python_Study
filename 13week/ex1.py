06bcb1557ad6ae6d45e675d6349c25a3eb8f3165
yangfaring <did030930@gmail.com>
2024-05-30 22:53:40 +0900

    Add files via upload

---
+++ 13week/ex1.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+
+air = pd.read_csv('air.csv')
+
+print("1***************************")
+print(air, end="\n\n")
+air.info() # 컬럼별 non-null 데이터 있는 수
+
+print("2***************************")
+print(air.isnull().sum(), end="\n\n") #결측치의 수
+#삭제
+print("3***************************")
+air_d = air.dropna(axis=0)
+print(air_d, end="\n\n")
+
+print("4***************************")
+air_d2 = air.dropna(axis=1)
+print(air_d2, end="\n\n")
+# 대체
+print("5***************************")
+air_m1 = air.fillna(0)
+print(air_m1, end="\n\n")
+
+print("6***************************")
+mean = air['PM10'].mean()
+air_m2 = air['PM10'].fillna(mean)
+print(air_m2, end="\n\n")
---
+++ 13week/ex2.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+
+data = {'열1': [1,2,2,3,4],
+        '열2': ['A','B','B','C','D'], 
+        '열3': [-10,20,30,40,150],
+        '열4': ['A','B','B','Z','Z']}
+
+df = pd.DataFrame(data)
+print(df , end="\n\n")
+
+print("1*****************")
+df.loc[df['열4']=='Z', '열4'] = 'F'
+print(df , end="\n\n")
+
+print("2*****************")
+df.loc[(df['열3'] > 100) | (df['열3'] < 0), '열3'] = 0
+print(df , end="\n\n")
+# 중복 데이터 
+print("3*****************")
+df.drop_duplicates(subset=['열2','열4'], keep='first', inplace=True)
+print(df , end="\n\n")
