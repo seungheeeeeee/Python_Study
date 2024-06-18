cc624e840029ae3c323b0121f4589ab4240805d1
yangfaring <did030930@gmail.com>
2024-06-18 14:10:41 +0900

    Add files via upload

---
+++ 14week/ex2.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+def addtext(x,y):
+    for i in range(len(x)):
+        plt.text(i,y[i]+0.5,y[i], ha = 'center')
+
+hat = pd.read_csv('ch4-1.csv')
+print(hat, end="\n\n")
+
+print(hat.head(),end="\n\n")
+
+plt.figure(figsize=(15,10))
+plt.bar(hat['hatchery'], hat['chick'], color = ('red','orange','yellow','green','blue','navy','purple'))
+
+plt.title('hatchery statistics')
+plt.xlabel('hatchery')
+plt.ylabel('click count')
+
+addtext(hat['hatchery'], hat['chick'])
+plt.show()
+
+pct = hat['chick']/hat['chick'].sum()
+col7 = sns.color_palette('Pastel2',7)
+
+plt.figure(figsize=(10,10))
+plt.pie(pct,labels = hat['hatchery'], autopct='%.1f%%', colors=col7,counterclock = False)
+
+plt.show()
